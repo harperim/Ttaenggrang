74fa6a448e4af8e7972a03caae227ad4cba801ae
최연지 <rokmc3038@naver.com>
2025-02-17 17:41:22 +0900

    feat: ãETFHistory 생성

---
+++ backend/src/main/java/com/ladysparks/ttaenggrang/domain/etf/dto/EtfHistoryDTO.java
@@ -0,0 +1,37 @@
+package com.ladysparks.ttaenggrang.domain.etf.dto;
+
+import com.ladysparks.ttaenggrang.domain.stock.entity.StockHistory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EtfHistoryDTO {
+    private Long id;
+    private int price;
+    private int buyVolume;
+    private int sellVolume;
+    private Timestamp date;
+
+    // 조인
+    // Etf 관련 (etf_id 외래 키를 참조)
+    private Long etfId;      // etf_id 외래 키
+
+
+    public static com.ladysparks.ttaenggrang.domain.stock.dto.StockHistoryDTO fromEntity(StockHistory stockHistory) {
+        return com.ladysparks.ttaenggrang.domain.stock.dto.StockHistoryDTO.builder()
+                .id(stockHistory.getId())
+                .price(stockHistory.getPrice())  // 가격
+                .buyVolume(stockHistory.getBuyVolume())  // 구매량
+                .sellVolume(stockHistory.getSellVolume())  // 판매량
+                .date(stockHistory.getCreatedAt())  // 거래 일자
+                .stockId(stockHistory.getStock() != null ? Long.valueOf(stockHistory.getStock().getId()) : null)  // 주식 ID
+                .etfId(stockHistory.getEtf() != null ? Long.valueOf(stockHistory.getEtf().getId()) : null)  // ETF ID
+                .build();
+        }
+}
